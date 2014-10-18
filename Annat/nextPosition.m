343afb48cc410c8a0e2d74c0c07245fee84aaad1
micnil <michaelnilsson90@hotmail.com>
2014-10-18 15:39:05 +0800

    removing things that are not needed for the gh-page

--- Annat/nextPosition.m
+++
@@ -1,9 +0,0 @@
-function [ new_pos, new_w, new_v ] = nextPosition(pos, w, v )
-
-new_v= v - (F_friktion/m)*dt;
-new_w = w - J*alpha*dt; 
-new_pos=pos + v*dt;
-
-
-end
-
--- Matlabsimulering/calculateNewPosition.m
+++
@@ -1,8 +0,0 @@
-function [ newStonePos, newStoneAngle ] = calculateNewPosition(prevStonePos,prevStoneAngle,velocity,angleSpeed,dt)
-%Calculates both coordinates and direction in radians of one stone
-        newStonePos = prevStonePos + velocity*dt;
-        newStoneAngle = prevStoneAngle + angleSpeed*dt;
-        if (newStoneAngle >= 2*pi)
-            newStoneAngle = 0; 
-        end
-end
